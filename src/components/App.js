ae5704b45aaca7a22d77f4678aeacce68823aed8
aptDroid <apt.sharp@gmail.com>
2018-07-09 00:47:21 +0530

    api fetch included

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,31 @@ import React, { Component } from 'react';
 import './App.scss';
 
 class App extends Component {
+  constructor(props) {
+	  super(props);
+	  
+	  this.state={repos: []};
+  }
+  
+  componentDidMount() {
+	  fetch('https://api.github.com/users/aptDroid/repos', {
+		  method: 'GET',
+	  }).then(result => result.json())
+	  .catch(error => console.erro('ERROR:', error))
+  .then(response => {
+		let getRepos = response.map((repo, index) => <li key={index}>{repo.name}</li>);
+		this.setState({
+			repos: getRepos
+			});
+		}
+	)
+  }
+  
   render() {
     return (
       <div id="cont">
         <h1 className="xyz">My React App!</h1>
+		{this.state.repos}
       </div>
     );
   }
