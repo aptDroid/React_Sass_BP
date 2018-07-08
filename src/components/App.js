12d7f18f3729a40ce7c45880c3f4bec03fcfe5b3
aptDroid <apt.sharp@gmail.com>
2018-07-08 23:55:16 +0530

    sass configured, output .css not configured

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="cont">
+        <h1 className="xyz">My React App!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
